02fb4772a355b305e0d007736430b6e0bcfc78ca
Sam Istephan <sami@microsoft.com>
2020-06-04 02:52:33 +0000

    added Python and R examples

---
+++ Pipeline Python and R/Scripts/rstep_launcher.R
@@ -0,0 +1,5 @@
+# This is the auto-generated launcher file.
+# It installs the packages specified in the step.
+# Once all the packages are successfully installed, it will execute the R script.
+
+source("helloworld.R")
